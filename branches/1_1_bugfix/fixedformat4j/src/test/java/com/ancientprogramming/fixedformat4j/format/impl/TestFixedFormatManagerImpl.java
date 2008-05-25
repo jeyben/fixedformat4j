421dfd1b3705226bca21c506eed52d5f1dd8471d
jacobvoneyben <jacobvoneyben@8d02eb0e-b647-0410-804d-e9a715892a94>
2008-05-25 20:21:36 +0000

    Added bugfix branch 1_1_bugfix

---
+++ branches/1_1_bugfix/fixedformat4j/src/test/java/com/ancientprogramming/fixedformat4j/format/impl/TestFixedFormatManagerImpl.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2004 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ancientprogramming.fixedformat4j.format.impl;
+
+import com.ancientprogramming.fixedformat4j.format.FixedFormatManager;
+import com.ancientprogramming.fixedformat4j.format.impl.FixedFormatManagerImpl;
+import com.ancientprogramming.fixedformat4j.format.impl.MultibleFieldsRecord;
+import com.ancientprogramming.fixedformat4j.format.impl.MyRecord;
+import com.ancientprogramming.fixedformat4j.exception.FixedFormatException;
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+import java.util.Calendar;
+import java.math.BigDecimal;
+
+/**
+ * @author Jacob von Eyben www.ancientprogramming.com
+ * @since 1.0.0
+ */
+public class TestFixedFormatManagerImpl extends TestCase {
+
+  private static String STR = "some text ";
+
+  public static final String MY_RECORD_DATA = "some text +012320080514CT+011+000001035+000002056-0012 0120";
+  public static final String MULTIBLE_RECORD_DATA = "some      2008101320081013                       +100";
+  public static final String MULTIBLE_RECORD_DATA_X_PADDED = "some      2008101320081013xxxxxxxxxxxxxxxxxxxxxxx+100";
+
+  FixedFormatManager manager = null;
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    manager = new FixedFormatManagerImpl();
+  }
+
+  public void testLoadRecord() {
+    MyRecord loadedRecord = manager.load(MyRecord.class, MY_RECORD_DATA);
+    Assert.assertNotNull(loadedRecord);
+    Assert.assertEquals(STR, loadedRecord.getStringData());
+    Assert.assertTrue(loadedRecord.getBooleanData());
+  }
+
+  public void testLoadMultibleFieldsRecord() {
+    //when reading data having multible field annotations the first field will decide what data to return
+    Calendar someDay = Calendar.getInstance();
+    someDay.set(2008, 9, 13, 0, 0, 0);
+    someDay.set(Calendar.MILLISECOND, 0);
+    MultibleFieldsRecord loadedRecord = manager.load(MultibleFieldsRecord.class, MULTIBLE_RECORD_DATA);
+    Assert.assertNotNull(loadedRecord);
+    Assert.assertEquals("some      ", loadedRecord.getStringData());
+    Assert.assertEquals(someDay.getTime(), loadedRecord.getDateData());
+  }
+
+  public void testExportRecordObject() {
+    Calendar someDay = Calendar.getInstance();
+    someDay.set(2008, 4, 14, 0, 0, 0);
+    someDay.set(Calendar.MILLISECOND, 0);
+
+    MyRecord myRecord = new MyRecord();
+    myRecord.setBooleanData(true);
+    myRecord.setCharData('C');
+    myRecord.setDateData(someDay.getTime());
+    myRecord.setDoubleData(10.35);
+    myRecord.setFloatData(20.56F);
+    myRecord.setLongData(11L);
+    myRecord.setIntegerData(123);
+    myRecord.setStringData("some text ");
+    myRecord.setBigDecimalData(new BigDecimal(-12.012));
+    Assert.assertEquals("wrong record exported", MY_RECORD_DATA, manager.export(myRecord));
+  }
+
+  public void testExportMultibleFieldRecordObject() {
+    Calendar someDay = Calendar.getInstance();
+    someDay.set(2008, 9, 13, 0, 0, 0);
+    someDay.set(Calendar.MILLISECOND, 0);
+
+    MultibleFieldsRecord multibleFieldsRecord = new MultibleFieldsRecord();
+    multibleFieldsRecord.setDateData(someDay.getTime());
+    multibleFieldsRecord.setStringData("some      ");
+    multibleFieldsRecord.setIntegerdata(100);
+    manager.export(multibleFieldsRecord);
+    Assert.assertEquals("wrong record exported", MULTIBLE_RECORD_DATA, manager.export(multibleFieldsRecord));
+  }
+
+  public void testExportIntoExistingString() {
+    Calendar someDay = Calendar.getInstance();
+    someDay.set(2008, 9, 13, 0, 0, 0);
+    someDay.set(Calendar.MILLISECOND, 0);
+
+    MultibleFieldsRecord multibleFieldsRecord = new MultibleFieldsRecord();
+    multibleFieldsRecord.setDateData(someDay.getTime());
+    multibleFieldsRecord.setStringData("some      ");
+    multibleFieldsRecord.setIntegerdata(100);
+    String exportedString = manager.export("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", multibleFieldsRecord);
+    Assert.assertEquals("wrong record exported", MULTIBLE_RECORD_DATA_X_PADDED, exportedString);
+  }
+
+  public void testLoadNonRecordAnnotatedClass() {
+    try {
+      manager.load(String.class, "some");
+    } catch (FixedFormatException e) {
+      //expected
+    }
+  }
+}
---
+++ branches/1_1_bugfix/samples/src/main/java/com/ancientprogramming/fixedformat4j/samples/basic/FixedRecordReadWriter.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2004 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ancientprogramming.fixedformat4j.samples.basic;
+
+import com.ancientprogramming.fixedformat4j.annotation.*;
+
+import java.util.Date;
+
+/**
+ * Using property annotations we only have to define our annotations once for both reading and writing
+ */
+@Record(length = 41)
+public abstract class FixedRecordReadWriter {
+
+  private Character recordType;
+
+  private Date date;
+
+  private Double amount;
+
+  private String description;
+
+  private Boolean ok;
+
+
+  @Field(offset = 1, length = 1)
+  public Character getRecordType() {
+    return recordType;
+  }
+
+
+  public void setRecordType(Character recordType) {
+    this.recordType = recordType;
+  }
+
+  @Field(offset = 2, length = 8)
+  @FixedFormatPattern("yyyyMMdd")
+  public Date getDate() {
+    return date;
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
+  @Field(offset = 10, length = 10, paddingChar = '0', align = Align.RIGHT)
+  @FixedFormatDecimal(useDecimalDelimiter = false)
+  public Double getAmount() {
+    return amount;
+  }
+
+  public void setAmount(Double amount) {
+    this.amount = amount;
+  }
+
+  @Field(offset = 20, length = 20)
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  @Field(offset = 40, length = 1)
+  @FixedFormatBoolean
+  public Boolean getOk() {
+    return ok;
+  }
+
+  public void setOk(Boolean ok) {
+    this.ok = ok;
+  }
+
+
+  public String toString() {
+    return "FixedRecordReadWriter{" +
+        "recordType=" + getRecordType() +
+        ", date=" + getDate() +
+        ", amount=" + getAmount() +
+        ", description='" + getDescription() + '\'' +
+        ", ok=" + getOk() +
+        '}';
+  }
+}
