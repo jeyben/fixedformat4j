1c2b675218a1819cb2eb3cb77a15458f28b4964a
jacobvoneyben <jacobvoneyben@8d02eb0e-b647-0410-804d-e9a715892a94>
2008-05-30 16:38:39 +0000

    Added simple sample application

---
+++ trunk/samples/src/main/java/com/ancientprogramming/fixedformat4j/samples/basic/BasicUsage.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2004 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ancientprogramming.fixedformat4j.samples.basic;
+
+import com.ancientprogramming.fixedformat4j.format.FixedFormatManager;
+import com.ancientprogramming.fixedformat4j.format.impl.FixedFormatManagerImpl;
+
+/**
+ * Shows the basic usage
+ *
+ * @author Jacob von Eyben www.ancientprogramming.com
+ * @since 1.2.0
+ */
+public class BasicUsage {
+
+  private static FixedFormatManager manager = new FixedFormatManagerImpl();
+
+  public static void main(String[] args) {
+    String string = "string    001232008-05-29";
+    BasicRecord record = manager.load(BasicRecord.class, string);
+
+    System.out.println("The parsed string: " + record.getStringData());
+    System.out.println("The parsed integer: " + record.getIntegerData());
+    System.out.println("The parsed date: " + record.getDateData());
+
+    record.setIntegerData(100);
+    System.out.println("Exported: " + manager.export(record));
+  }
+}
